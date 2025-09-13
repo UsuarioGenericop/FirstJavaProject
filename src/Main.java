6f3866b18c1aa46eaac42d0b54097c9d20dba50b
Andres <andydquintero@gmail.com>
2025-09-13 00:00:59 -0500

    AddedFiles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[]args){
+        System.out.print("hello\n");
+        System.out.print("Hello wold\n");
+        System.out.print("Hello world!");//First Java Program
+
+    }
+}
